f44c8ba00d29bf032c9014aac0ad3321a895b43b
Devin Nemec <devin.nemec@enterprisedb.com>
2020-08-25 11:17:50 -0500

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,20 @@ then
   DESTINATION_BRANCH=master
 fi
 
-env | grep -v API_TOKEN_GITHUB
+sort | env | grep -v API_TOKEN_GITHUB
 
 CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
 ls -la
-git config --global user.email "$USER_EMAIL"
+git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone --single-branch --branch $DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$GITHUB_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 echo "Checking cloned repo"
 ls -la "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-cp "$SOURCE_FILE" "$CLONE_DIR/$DESTINATION_FOLDER"
+cp "$SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 cd "$CLONE_DIR"
 ls -la
 
@@ -28,4 +28,4 @@ git status
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 
 echo "Pushing git commit"
-git push origin $DESTINATION_BRANCH
+git push origin $INPUT_DESTINATION_BRANCH
