af559fe54d654299d9015368a4548ebf1eccfbfe
Devin Nemec <devin.nemec@enterprisedb.com>
2020-08-26 07:35:34 -0500

    remove excess logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,23 +14,17 @@ then
   INPUT_DESTINATION_BRANCH=master
 fi
 
-env | grep -v API_TOKEN_GITHUB | sort
-
 CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
-ls -la
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
-echo "Checking cloned repo"
-ls -la "$CLONE_DIR"
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 cd "$CLONE_DIR"
-ls -la
 
 echo "Adding git commit"
 git add .
