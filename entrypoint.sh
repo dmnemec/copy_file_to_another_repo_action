535ed0ac28e35582a232382f72ca347f2fa2ba4e
Vangelis Katikaridis <v.katikaridis@thebeat.co>
2020-11-03 12:26:31 +0100

    add quotes for message

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ echo "Adding git commit"
 git add .
 if git status | grep -q "Changes to be committed"
 then
-  git commit --message $INPUT_COMMIT_MESSAGE
+  git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
   git push -u origin HEAD:$OUTPUT_BRANCH
 else
