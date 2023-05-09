da9e2330cf61cd3cbb86c82a8f0be6df421efaee
Dobbelina2 <132213227+Dobbelina2@users.noreply.github.com>
2023-05-09 23:51:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,16 @@
 set -e
 set -x
 
-if [ -z "$INPUT_SOURCE_FILE" ]
-then
+if [ -z "$INPUT_SOURCE_FILE" ]; then
   echo "Source file must be defined"
   return 1
 fi
 
-if [ -z "$INPUT_GIT_SERVER" ]
-then
+if [ -z "$INPUT_GIT_SERVER" ]; then
   INPUT_GIT_SERVER="github.com"
 fi
 
-if [ -z "$INPUT_DESTINATION_BRANCH" ]
-then
+if [ -z "$INPUT_DESTINATION_BRANCH" ]; then
   INPUT_DESTINATION_BRANCH=main
 fi
 OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
@@ -27,8 +24,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-if [ ! -z "$INPUT_RENAME" ]
-then
+if [ ! -z "$INPUT_RENAME" ]; then
   echo "Setting new filename: ${INPUT_RENAME}"
   DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
 else
@@ -36,9 +32,12 @@ else
 fi
 
 echo "Copying contents to git repo"
+if [ ! -z "INPUT_DELETE_EXISTING" ]; then
+  echo "Deleting existing files"
+  rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER
+fi
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-if [ -z "$INPUT_USE_RSYNC" ]
-then
+if [ -z "$INPUT_USE_RSYNC" ]; then
   cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
 else
   echo "rsync mode detected"
@@ -47,22 +46,19 @@ fi
 
 cd "$CLONE_DIR"
 
-if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
-then
+if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]; then
   echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
   git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
   OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
 fi
 
-if [ -z "$INPUT_COMMIT_MESSAGE" ]
-then
+if [ -z "$INPUT_COMMIT_MESSAGE" ]; then
   INPUT_COMMIT_MESSAGE="Update from https://$INPUT_GIT_SERVER/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
 fi
 
 echo "Adding git commit"
 git add .
-if git status | grep -q "Changes to be committed"
-then
+if git status | grep -q "Changes to be committed"; then
   git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
   git push -u origin HEAD:"$OUTPUT_BRANCH"
